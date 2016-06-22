42015388512309752e6adfba6c576e8ecd037270
柴瑞军 <keinrain@163.com>
2016-06-22 16:18:45 +0800

    初始化工程

---
+++ tamade/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tamade
+//
+//  Created by 柴瑞军 on 16/6/22.
+//  Copyright © 2016年 com.pingan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tamade/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tamade
+//
+//  Created by 柴瑞军 on 16/6/22.
+//  Copyright © 2016年 com.pingan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
