63e89099c45d2004584abd33f4f6a1c14e4c5f11
柴瑞军 <keinrain@163.com>
2016-06-22 16:40:47 +0800

    测试5

--- tamade/ViewController.m
+++ tamade/ViewController.m
@@ -33,4 +33,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(void)mytext{
+    NSLog(@"jintiannidouganleshenmene,qishi woyebuzhidao");
+    //创建Button
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
+    [btn setTitle:@"点击我吧" forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
+    
+    //创建UILabel
+    UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
+    lab.text = @"nijishigesdashanguan";
+    [self.view addSubview:lab];
+    
+    //创建tableview
+    UITableView *tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, 100, 100) style:UITableViewStylePlain];
+    tableView.sectionHeaderHeight = 100;
+    tableView.sectionFooterHeight = 200;
+    [self.view addSubview:tableView];
+    
+    
+    tableView.scrollEnabled = NO;
+    
+    
+    //创建核心动画
+    
+}
+
 @end
